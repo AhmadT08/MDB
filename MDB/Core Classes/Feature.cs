0e42e8e7598c4e2b6ec8e1b364a9e2efc30793b7
WRostom <rostomrevel@gmail.com>
2017-04-11 19:01:39 +0200

    re-add sorted files

---
+++ MDB/Core Classes/Feature.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MDB
+{
+    class Feature
+    {
+        public string ActingRole { get; set; }
+
+        public Watchable Entity { get; set; }
+
+        public Person ThePerson { get; set; }
+
+        public string ProductionRole { get; set; }
+
+        public Feature(string actingRole, Watchable entity, Person thePerson, string productionRole)
+        {
+            ActingRole = actingRole;
+            Entity = entity;
+            ThePerson = thePerson;
+            ProductionRole = productionRole;
+        }
+
+        public Feature()
+        {
+
+        }
+
+
+    }
+}
