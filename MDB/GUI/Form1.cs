8a625951b2dbb368e20bbd235a19da60e27e4092
WRostom <rostomrevel@gmail.com>
2017-04-15 17:05:34 +0200

    add hover and click to dynamic pictureBox

--- MDB/GUI/Form1.cs
+++ MDB/GUI/Form1.cs
@@ -9,6 +9,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using MDB.GUI;
+
 namespace MDB
 {
     public partial class Form1 : Form
@@ -28,12 +30,20 @@ namespace MDB
 
         private void createPictureBoxandTitle(Image poster, String title, String type)
         {
+            Panel panel = new Panel();
+            panel.Size = new System.Drawing.Size(150, 220);
+            panel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
+
             PictureBox picturebox = new PictureBox();
             picturebox.MaximumSize = new Size(145, 215);
+            picturebox.Location = new Point(2, 2);
             picturebox.Size = new Size(145, 215);
             picturebox.SizeMode = PictureBoxSizeMode.StretchImage;
             picturebox.Image = poster;
             picturebox.TabStop = false;
+            picturebox.MouseHover += new System.EventHandler(this.picturebox_MouseHover);
+            picturebox.MouseLeave += new System.EventHandler(this.picturebox_MouseLeave);
+            picturebox.Click += new System.EventHandler(this.picturebox_Click);
 
             Label ItemTitle = new Label();
             ItemTitle.AutoSize = true;
@@ -47,12 +57,14 @@ namespace MDB
             ItemTitle.TextAlign = ContentAlignment.TopCenter;
             if (type == "Movie")
             {
-                picturebox.Location = new Point(moviePicturePosition, 0);
+                panel.Location = new System.Drawing.Point(moviePicturePosition, 3);
+                panel.Name = "WrappermovieBox" + moviePictureBoxCounter;
                 picturebox.Name = "movieBox" + moviePictureBoxCounter;
                 picturebox.TabIndex = moviePictureBoxCounter;
                 ItemTitle.Location = new Point(movieTitlePosition, 218);
                 ItemTitle.Name = "mLabel" + moviePictureBoxCounter;
-                panel14.Controls.Add(picturebox);
+                panel.Controls.Add(picturebox);
+                panel14.Controls.Add(panel);
                 panel14.Controls.Add(ItemTitle);
                 moviePictureBoxCounter++;
                 moviePicturePosition += 156;
@@ -60,17 +72,37 @@ namespace MDB
             }
             else if (type == "Show")
             {
-                picturebox.Location = new Point(showPicturePosition, 0);
-                picturebox.Name = "movieBox" + showPictureBoxCounter;
+                panel.Location = new System.Drawing.Point(showPicturePosition, 3);
+                panel.Name = "WrappershowBox" + moviePictureBoxCounter;
+                picturebox.Name = "showBox" + showPictureBoxCounter;
                 picturebox.TabIndex = showPictureBoxCounter;
                 ItemTitle.Location = new Point(showTitlePosition, 218);
-                ItemTitle.Name = "mLabel" + showPictureBoxCounter;
-                panel2.Controls.Add(picturebox);
+                ItemTitle.Name = "sLabel" + showPictureBoxCounter;
+                panel.Controls.Add(picturebox);
+                panel2.Controls.Add(panel);
                 panel2.Controls.Add(ItemTitle);
                 showPictureBoxCounter++;
                 showPicturePosition += 156;
                 showTitlePosition += 152;
             }
+        }
+
+        private void picturebox_Click(object sender, EventArgs e)
+        {
+            ShowMovie showMovie = new ShowMovie();
+            showMovie.ShowDialog();
+        }
+
+        private void picturebox_MouseLeave(object sender, EventArgs e)
+        {
+            PictureBox op = (PictureBox)sender;
+            op.Parent.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
+        }
+
+        private void picturebox_MouseHover(object sender, EventArgs e)
+        {
+            PictureBox op = (PictureBox)sender;
+            op.Parent.BackColor = System.Drawing.Color.Yellow;
         }
 
         private void AddToPanel(PictureBox picturebox, Label ItemTitle)
@@ -114,5 +146,11 @@ namespace MDB
         {
 
         }
+
+        private void panel1_MouseMove(object sender, EventArgs e)
+        {
+            Console.WriteLine("Hello");
+        }
+
     }
 }
